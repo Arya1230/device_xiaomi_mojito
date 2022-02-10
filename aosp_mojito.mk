6c7a6088d747cdb6085e18ad59aa62a18a6b9306
Arya1230 <arya1230sarkar@gmail.com>
2022-02-11 01:35:27 +0530

    PPUI Bring Up

--- aosp_mojito.mk
+++ aosp_mojito.mk
@@ -11,10 +11,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from mojito device
 $(call inherit-product, device/xiaomi/mojito/device.mk)
 
-# Inherit some common PixelExperience stuff
+# Inherit some common PPUI stuff
+IS_PHONE := true
 TARGET_USES_AOSP_RECOVERY := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
+CUSTOM_BUILD_TYPE := OFFICIAL
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+TARGET_INCLUDE_STOCK_ACORE := false
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
@@ -25,3 +29,7 @@ PRODUCT_MODEL := Redmi Note 10
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# PPUI Props
+ro.pixelplusui.maintainer=Arya1230
+org.pixelplusui.device=RedmiNote10
